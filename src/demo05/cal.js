a191cd905c7befdc3c89c776720abd72640f687c
邓启明 <dengqiming@xiaomadada.com>
2017-07-31 21:55:56 +0800

    测试demo

--- src/demo05/cal.js
+++ src/demo05/cal.js
@@ -15,7 +15,7 @@ let realRate = rate + quan/money/pDays*year*100;
 
 console.log('项目天数：' + pDays + '天', '年化：' + Math.round(realRate * 100, 5) / 100 + '%', '持有' + days + '天可转让');
 console.log('投资金额：' + Math.round(money * 100, 4) / 100 + '元', '返现券：' + Math.round(quan * 100, 4) / 100 + '元');
-console.log('变现天数：' + zrDays + '天', '变现手续费：变现金额×' + zrCost + '%')
+console.log('变现天数：' + zrDays + '天', '变现手续费：变现金额×' + zrCost + '%');
 console.log('-------------------------------------');
 for(var i = 0; i < 10; i++) {
     let lixi = money*rate/100 * pDays/year;

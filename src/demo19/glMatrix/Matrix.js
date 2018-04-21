65d050285cf1907e69061f0a60e049ad919bf3f2
dengqiming <dengqiming@ddy98.com>
2018-04-21 21:10:13 +0800

    矩阵变换

--- src/demo19/glMatrix/Matrix.js
+++ src/demo19/glMatrix/Matrix.js
@@ -1,7 +1,11 @@
 import {mat3} from 'gl-matrix';
 import {Vector} from './Vector';
 
+/**
+ * 矩阵
+ */
 class Matrix {
+  // 静态工厂
   static create(matrix) {
     if (matrix instanceof Array) {
       return new Matrix(matrix);
@@ -9,20 +13,21 @@ class Matrix {
       return Matrix.identity();
     }
   }
-
+  // 单位矩阵
   static identity() {
     return new Matrix(mat3.create());
   }
-
+  // 单位矩阵
   static I() {
     return Matrix.identity();
   }
-
+  // 构造函数,数组
   constructor(matrix) {
     this.matrix = mat3.fromValues(...matrix);
   }
-
+  // 平移
   translate(dx, dy) {
+    // 平移矩阵
     const matrix = mat3.fromValues(
       1, 0, 0,
       0, 1, 0,
@@ -32,8 +37,9 @@ class Matrix {
     mat3.multiply(out, matrix, this.matrix);
     return new Matrix(out);
   }
-
+  // 旋转
   rotate(theta) {
+    // 旋转矩阵
     const matrix = mat3.fromValues(
       Math.cos(theta), Math.sin(theta), 0,
       -Math.sin(theta), Math.cos(theta), 0,
@@ -43,8 +49,9 @@ class Matrix {
     mat3.multiply(out, matrix, this.matrix);
     return new Matrix(out);
   }
-
+  // 缩放
   scale(sx, sy) {
+    // 缩放矩阵
     const matrix = mat3.fromValues(
       sx, 0, 0,
       0, sy, 0,
@@ -54,32 +61,32 @@ class Matrix {
     mat3.multiply(out, matrix, this.matrix);
     return new Matrix(out);
   }
-
+  // 矩阵乘法
   multiply(matrix) {
     const out = mat3.create();
-    mat3.multiply(out, this.matrix, matrix.matrix);
+    mat3.multiply(out, matrix.matrix, this.matrix);
     return new Matrix(out);
   }
-
+  // 矩阵乘法
   x(matrix) {
     return this.multiply(matrix);
   }
-
+  // 逆矩阵
   invert() {
     const out = mat3.create();
     mat3.invert(out, this.matrix);
     return new Matrix(out);
   }
-
+  // 获取矩阵的某一行
   row(index) {
     const start = 3 * (index - 1);
     return Vector.create([this.matrix[start], this.matrix[start + 1], this.matrix[start + 2]]);
   }
-
+  // 获取矩阵元素
   elements() {
     return this.matrix;
   }
-
+  // toString方法
   toString() {
     return this.matrix.toString();
   }
--- src/demo19/glMatrix/Vector.js
+++ src/demo19/glMatrix/Vector.js
@@ -1,26 +1,30 @@
 import {vec2, vec3} from 'gl-matrix';
 
+/**
+ * 向量组
+ */
 class Vectors {
+  // 静态工厂
   static create(vectors) {
     if (vectors instanceof Array) {
       return new Vectors(vectors);
     }
   }
-
+  // 构造函数,二维数组
   constructor(vectors) {
     this.vectors = vectors.map((vector) => {
       return Vector.create(vector);
     });
   }
-
+  // 获取向量组的某一行
   row(index) {
     return this.vectors[index - 1];
   }
-
+  // 获取向量组元素
   elements() {
     return this.vectors;
   }
-
+  // toString方法
   toString() {
     return this.vectors.map((vector) => {
       return vector.toString();
@@ -28,7 +32,11 @@ class Vectors {
   }
 }
 
+/**
+ * 向量
+ */
 class Vector {
+  // 静态工厂
   static create(vector) {
     if (vector instanceof Array) {
       return new Vector(vector);
@@ -36,33 +44,33 @@ class Vector {
       return Vector.identity();
     }
   }
-
+  // 空向量
   static identity() {
     return new Vector(vec3.create());
   }
-
+  // 构造函数,数组
   constructor(vector) {
     this.vector = vec3.fromValues(...vector);
   }
-
+  // 向量变换,矩阵作用于向量
   transformMat3(matrix) {
     const out = vec3.create();
     vec3.transformMat3(out, this.vector, matrix.matrix);
     return new Vector(out);
   }
-
+  // 向量变换
   x(matrix) {
     return this.transformMat3(matrix);
   }
-
+  // 获取向量的某个元素
   e(index) {
     return this.vector[index - 1];
   }
-
+  // 获取向量元素
   elements() {
     return this.vector;
   }
-
+  // toString方法
   toString() {
     return this.vector.toString();
   }
